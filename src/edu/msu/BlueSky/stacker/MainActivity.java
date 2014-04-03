8cd0d32f4799c271d4d301ea09768f9dcf8f7128
Josh-Pelcher <pelch96@gmail.com>
2014-04-03 16:19:03 -0400

    Added to layouts, strings, and CreateUserActivity

--- src/edu/msu/BlueSky/stacker/MainActivity.java
+++ src/edu/msu/BlueSky/stacker/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_log_in);
 	}
 	
 	public void onStartGame(View view) {
@@ -24,5 +24,11 @@ public class MainActivity extends Activity {
 		intent.putExtra ( "player1Name", player1Name );
 		intent.putExtra ( "player2Name", player2Name );
 		startActivity(intent);
+	}
+	
+	public void onCreateUser(View view) {
+		Intent intent = new Intent(this, CreateUserActivity.class);
+		startActivity(intent);
+		
 	}
 }
